35e5575e5ad23f67af954230a62ecc27305a31f2
Ngugi Isaac Kinyanjui <itsngugiisaackinyanjui@gmail.com>
2024-02-02 08:06:38 +0300

    Update

--- LoginPage.java
+++ LoginPage.java
@@ -1,13 +1,15 @@
-// Importing packages
+// Importing necessary packages
 import javax.swing.JOptionPane;
 import java.util.HashMap;
 import java.util.Map;
 
+// User class to store user details
 class User {
     String username;
     String admission;
     String password;
 
+    // User constructor
     User(String username, String admission, String password) {
         this.username = username;
         this.admission = admission;
@@ -15,11 +17,14 @@ class User {
     }
 }
 
+// LoginPage class for handling user sign up and login
 public class LoginPage {
 
+    // HashMap to store User objects with their username as the key
     Map<String, User> userMap = new HashMap<>();
     String username, fullname, admission, password;
 
+    // SignUp method for new users
     public void SignUp() {
         // Input User Name
         username = JOptionPane.showInputDialog("Enter UserName: ");
@@ -30,11 +35,12 @@ public class LoginPage {
         // Input Password
         password = JOptionPane.showInputDialog("Enter password: ");
 
-        // Store username, admmission, password
+        // Create new User object and store it in the HashMap
         User user = new User(username, admission, password);
         userMap.put(username, user);
     }
 
+    // Login method for existing users
     public void Login() {
         // Input UserName
         username = JOptionPane.showInputDialog("Enter UserName: ");
@@ -42,20 +48,23 @@ public class LoginPage {
         // Input Password
         password = JOptionPane.showInputDialog("Enter Password: ");
 
-        // Validation
-        if(user.containsKey(username) && user.get(username).equals(password)) {
-                JOptionPane.showMessageDialog(null, "Successfully logged in. Welcome, " + username + "!");
-            } else {
-                JOptionPane.showMessageDialog(null, "Wrong username or password. Please try again.");
-            }
+        // Validate the username and password
+        if(userMap.containsKey(username) && userMap.get(username).password.equals(password)) {
+            JOptionPane.showMessageDialog(null, "Successfully logged in. Welcome, " + username + "!");
+        } else {
+            JOptionPane.showMessageDialog(null, "Wrong username or password. Please try again.");
+        }
     }
     
+    // Main method
     public static void main(String[] args) {
         LoginPage loginPage = new LoginPage();
+        // Ask the user if they want to sign up
         int option = JOptionPane.showConfirmDialog(null, "Would you like to sign up?", "Choose", JOptionPane.YES_NO_OPTION);
         if (option == JOptionPane.YES_OPTION) {
             loginPage.SignUp();
         }
+        // Call the Login method
         loginPage.Login();
     }
 }
