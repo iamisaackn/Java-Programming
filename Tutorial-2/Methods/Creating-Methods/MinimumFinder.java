945b33a9127c305fef6b5d43a8b33cbe98510aaf
Ngugi Isaac Kinyanjui <itsngugiisaackinyanjui@gmail.com>
2024-02-12 12:10:12 +0300

    Update

---
+++ Tutorial-2/Methods/Creating-Methods/MinimumFinder.java
@@ -0,0 +1,15 @@
+public class MinimumFinder {
+    public static int minimum(int n1, int n2) {
+        return (n1 > n2) ? n2 : n1;
+    }
+
+    public static void main(String[] args) {
+        int n1, n2, result;
+
+        n1 = 42;
+        n2 = 60;
+
+        result = minimum(n1, n2);
+        System.out.println("The minimum number is" + result);
+    }
+}
